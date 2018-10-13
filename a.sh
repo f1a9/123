7b573ac6564ebce99f797a97b28b9ba514c14301
YPBlib <ypb1024@foxmail.com>
2018-10-13 13:16:28 +0800

    fix CRLF

---
+++ a.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+A=B
+echo "PID for a.sh before exec/source/fork: $$"
+
+export A
+
+
+echo "in a.sh, \$A = $A"
+
+case $1 in
+    exec)
+        echo 'using exec ...'
+        exec ./b.sh ;;
+    source)
+        echo 'using source ...'
+        source ./b.sh ;;
+    *)
+        echo 'using fork by default ...'
+        ./b.sh
+esac
+
+echo "PID of a.sh afterwards : $$"
+echo "a.sh: \$A = $A"
---
+++ b.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "PID for b.sh: $$"
+echo "b.sh get \$A=$A from a.sh"
+A=C
+export A
+echo "in b.sh, \$A=$A "
